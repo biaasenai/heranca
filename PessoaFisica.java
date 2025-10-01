5252126f59daef777c51b518bdda9ad251535042
Bia <cvieira.bia@gmail.com>
2025-10-01 12:28:09 -0300

    aula01/10

---
+++ PessoaFisica.java
@@ -0,0 +1,24 @@
+package Atividade1;
+
+public class PessoaFisica extends Pessoa {
+	
+	private String cpf;
+	private String rg;
+	
+	public PessoaFisica(String nome, String telefone, String cpf, String rg) {
+		super(nome, telefone);
+		this.cpf = cpf;
+		this.rg = rg;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public String getRg() {
+		return rg;
+	}
+
+	
+
+}
